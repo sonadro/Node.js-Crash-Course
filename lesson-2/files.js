49833a6a627588b32cb0e095052d02660519c699
sonadro <sondre.rooth@gmail.com>
2022-11-29 13:48:28 +0100

    lesson-5 finish

---
+++ lesson-2/files.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+// reading files
+// fs.readFile('./docs/blog1.txt', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// console.log('last line');
+
+// writing files
+// fs.writeFile('./docs/blog2.txt', 'hello world', () => {
+//     console.log('file written');
+// });
+
+// directories
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', err => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('folder created');
+    });
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('folder deleted');
+    });
+}
+
+// deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('file removed');
+    });
+}
---
+++ lesson-2/global.js
@@ -0,0 +1,18 @@
+// global object
+
+// console.log(global);
+
+// console.log('before the timeout');
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+// console.log('after the timeout');
+
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
