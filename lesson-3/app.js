822529dd629803e124a6cb072de3a67279343766
sonadro <sondre.rooth@gmail.com>
2022-11-30 10:46:05 +0100

    Lesson-6

--- lesson-3/app.js
+++ lesson-3/app.js
@@ -21,5 +21,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(418).sendFile('./views/404.html', { root: __dirname });
 });
