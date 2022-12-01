e8b099c4c83e9b99afa600f149c115a2372523b7
sonadro <sondre.rooth@gmail.com>
2022-12-01 11:03:03 +0100

    Lesson #7

--- lesson-3/app.js
+++ lesson-3/app.js
@@ -3,15 +3,27 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
 });
 
 // redirects
@@ -21,5 +33,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(418).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: 'Page not found' });
 });
