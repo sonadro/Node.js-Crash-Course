033345cbbb7b53075961c6fc943263ac606d4ee2
sonadro <sondre.rooth@gmail.com>
2022-12-01 11:31:12 +0100

    Lesson 8

--- lesson-3/app.js
+++ lesson-3/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,6 +10,13 @@ app.set('view engine', 'ejs');
 // listen for requests
 app.listen(3000);
 
+// Static files
+app.use(express.static('public'));
+
+// Third-party middleware
+app.use(morgan('dev'));
+
+// Serve sites
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
